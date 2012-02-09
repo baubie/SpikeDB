8e1a25faa827d5295791104986c501d9e46e05e5
Brandon Aubie <brandon@aubie.ca>
2012-02-08 23:21:24 -0500

    Update mean spike count

--- src/plugins/meanSpikeCount.py
+++ src/plugins/meanSpikeCount.py
@@ -1,56 +1,141 @@
 ### Spike Count Analysis
+try:
+	from scipy import stats
+	hasSciPy = True
+except ImportError:
+	hasSciPy = False
 
 
 def SpikeDBAdvanced():
-	SpikeDB.addOptionRadio("type", ["Mean Spike Count", "Median Spike Count", "Shapiro-Wilk Test"], "Analysis Type", 0)
+	typeOptions = ["Mean SpikeCount", "Median Spike Count"]
+	if hasSciPy:
+		typeOptions.append("Shapiro-Wilk Test")
+
+	SpikeDB.addOptionRadio("type", typeOptions, "Analysis Type", 0)
 	SpikeDB.addRuler()
 	SpikeDB.addOptionCheckbox("showLimits", "Draw Minimum and Maximum Lines (Single File Only)", True)
 	SpikeDB.addOptionCheckbox("showAllPoints", "Draw spike counts for all trials (Single File Only)", True)
+
+def ShapiroWilk(f):
+	values = []
+	x = []
+
+	for t in f['trials']:
+		count = []
+		x.append(t['xvalue'])
+		for p in t['passes']:
+			count.append(len(p))
+		W, p = stats.shapiro(count)
+		values.append(p)
+	return x, values
+
+def meanSpikeCount(f):
+	means = []
+	err = []
+	x = []
+	minCount = []
+	maxCount = []
+	allPointsX = []
+	allPointsY = []
+	for t in f['trials']:
+		tmpMin = 10000000000;
+		tmpMax = 0;
+		count = []
+		x.append(t['xvalue'])
+		for p in t['passes']:
+			count.append(len(p))
+			if len(p) > tmpMax: tmpMax = len(p)
+			if len(p) < tmpMin: tmpMin = len(p)
+			allPointsY.append(len(p))
+			allPointsX.append(t['xvalue'])
+		minCount.append(tmpMin)
+		maxCount.append(tmpMax)
+		means.append(SpikeDB.mean(count))
+		err.append(SpikeDB.stddev(count))
+	return x, means, err, minCount, maxCount, allPointsX, allPointsY
+
+def medianSpikeCount(f):
+	means = []
+	medians = []
+	err = []
+	x = []
+	minCount = []
+	maxCount = []
+	allPointsX = []
+	allPointsY = []
+	for t in f['trials']:
+		tmpMin = 10000000000;
+		tmpMax = 0;
+		count = []
+		x.append(t['xvalue'])
+		for p in t['passes']:
+			count.append(len(p))
+			if len(p) > tmpMax: tmpMax = len(p)
+			if len(p) < tmpMin: tmpMin = len(p)
+			allPointsY.append(len(p))
+			allPointsX.append(t['xvalue'])
+		minCount.append(tmpMin)
+		maxCount.append(tmpMax)
+		median = 0
+		if len(count) > 0:
+			if len(count) % 2 == 1:
+				median = count[int(len(count) * 0.5 - 0.5)]
+			else:
+				median = SpikeDB.mean([count[int(len(count) * 0.5)-1], count[int(len(count) * 0.5)]])
+		medians.append(median)
+		err.append(SpikeDB.stddev(count))
+	return x, medians, err, minCount, maxCount, allPointsX, allPointsY
+
 
 def SpikeDBRun():
 	files = SpikeDB.getFiles(True)
 	options = SpikeDB.getOptions()
 
 	for f in files:
+		x = []
 		means = []
 		err = []
-		x = []
 		minCount = []
 		maxCount = []
 		allPointsX = []
 		allPointsY = []
-		for t in f['trials']:
-			tmpMin = 10000000000;
-			tmpMax = 0;
-			count = []
-			x.append(t['xvalue'])
-			for p in t['passes']:
-				count.append(len(p))
-				if len(p) > tmpMax: tmpMax = len(p)
-				if len(p) < tmpMin: tmpMin = len(p)
-				allPointsY.append(len(p))
-				allPointsX.append(t['xvalue'])
-			minCount.append(tmpMin)
-			maxCount.append(tmpMax)
-			means.append(SpikeDB.mean(count))
-			err.append(SpikeDB.stddev(count))
-		SpikeDB.plotSetRGBA(0,0,0,1)
-		SpikeDB.plotXLabel(f['xvar'])
-		SpikeDB.plotYLabel('Mean Spike Count')
-		SpikeDB.plotLine(x,means,err)
-		SpikeDB.plotYMin(0)
 
-		if options["showLimits"] and len(files) == 1:
-			SpikeDB.plotSetRGBA(1,0,0,0.15)
-			SpikeDB.plotLine(x,minCount,[])
-			SpikeDB.plotSetRGBA(0,0,1,0.15)
-			SpikeDB.plotLine(x,maxCount,[])
+		if options["type"] == 0:
+			x, means, err, minCount, maxCount, allPointsX, allPointsY = meanSpikeCount(f)
+			SpikeDB.plotSetRGBA(0,0,0,1)
+			SpikeDB.plotXLabel(f['xvar'])
+			SpikeDB.plotYLabel('Mean Spike Count')
+			SpikeDB.plotLine(x,means,err)
+			SpikeDB.plotYMin(0)
 
-		if options["showAllPoints"] and len(files) == 1:
-			SpikeDB.plotSetRGBA(0,0,0,0.2)
-			SpikeDB.plotSetLineWidth(0)
-			SpikeDB.plotSetPointSize(4)
-			SpikeDB.plotLine(allPointsX, allPointsY, [])
+		if options["type"] == 1:
+			x, medians, err, minCount, maxCount, allPointsX, allPointsY = medianSpikeCount(f)
+			SpikeDB.plotSetRGBA(0,0,0,1)
+			SpikeDB.plotXLabel(f['xvar'])
+			SpikeDB.plotYLabel('Median Spike Count')
+			SpikeDB.plotLine(x,medians,err)
+			SpikeDB.plotYMin(0)
+
+		if options["type"] == 2:
+			x, values = ShapiroWilk(f)
+			SpikeDB.plotSetRGBA(0,0,0,1)
+			SpikeDB.plotXLabel(f['xvar'])
+			SpikeDB.plotYLabel('Shapiro-Wilk p-value')
+			SpikeDB.plotLine(x,values,[])
+			SpikeDB.plotYMin(0)
+
+		if options["type"] == 0 or options["type"] == 1:
+			if options["showLimits"] and len(files) == 1:
+				SpikeDB.plotSetRGBA(1,0,0,0.15)
+				SpikeDB.plotLine(x,minCount,[])
+				SpikeDB.plotSetRGBA(0,0,1,0.15)
+				SpikeDB.plotLine(x,maxCount,[])
+
+			if options["showAllPoints"] and len(files) == 1:
+				SpikeDB.plotSetRGBA(0,0,0,0.2)
+				SpikeDB.plotSetLineWidth(0)
+				SpikeDB.plotSetPointSize(4)
+				SpikeDB.plotLine(allPointsX, allPointsY, [])
 
 
 
